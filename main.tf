0772fb7b9f8b896f855f917aa5303e6751eb7e39
sthakur1985 <104225376+sthakur1985@users.noreply.github.com>
2025-07-23 22:04:00 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+module "bucket_a" {
+  source         = "./modules/s3_bucket"
+  bucket_name    = "source-bucket-a"
+  lambda_arn     = module.lambda.lambda_arn
+  lambda_permission = module.lambda.lambda_permission
+}
+
+module "bucket_b" {
+  source      = "./modules/s3_bucket"
+  bucket_name = "destination-bucket-b"
+  lambda_arn  = "" # no lambda triggers
+  lambda_permission = null
+}
+
+module "iam" {
+  source        = "./modules/iam_role"
+  name          = "exif-stripper-lambda-role"
+  bucket_a_arn  = module.bucket_a.bucket_id
+  bucket_b_arn  = module.bucket_b.bucket_id
+}
+
+module "lambda" {
+  source            = "./modules/lambda_function"
+  function_name     = "exif-stripper"
+  role_arn          = module.iam.role_arn
+  source_bucket_arn = module.bucket_a.bucket_id
+}
