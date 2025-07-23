4984d250e610f1e8475c3edae717bc6e4962e4da
sthakur1985 <104225376+sthakur1985@users.noreply.github.com>
2025-07-23 23:47:32 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket"
+    bucket         = "terraform-st-bucket-0725"
     key            = "s3-exif-stripper/terraform.tfstate"
     region         = "eu-west-2"
     encrypt        = true
