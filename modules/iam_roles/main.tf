12e2027c5b3b7de2890b8b53aea7668b5a2a191f
sthakur1985 <104225376+sthakur1985@users.noreply.github.com>
2025-07-24 00:09:43 +0100

    Update main.tf

--- modules/iam_roles/main.tf
+++ modules/iam_roles/main.tf
@@ -4,7 +4,7 @@ resource "aws_iam_role" "lambda_function_r" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-data "aws_iam_policy_document" "assume_role_r" {
+data "aws_iam_policy_document" "assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
 
@@ -40,7 +40,7 @@ data "aws_iam_policy_document" "lambda_r" {
 
 resource "aws_iam_role_policy_attachment" "attach_r" {
   policy_arn = aws_iam_policy.lamda_r.arn
-  role       = aws_iam_role.lambda_function.name
+  role       = aws_iam_role.lambda_function_r.name
 }
 
 resource "aws_iam_role" "lambda_function_rw" {
@@ -48,20 +48,11 @@ resource "aws_iam_role" "lambda_function_rw" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-data "aws_iam_policy_document" "assume_role_rw" {
-  statement {
-    actions = ["sts:AssumeRole"]
 
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-  }
-}
 
 resource "aws_iam_policy" "lambda_rw" {
   name   = "${var.name}-policy"
-  policy = data.aws_iam_policy_document.lambda.json
+  policy = data.aws_iam_policy_document.lambda_rw.json
 }
 
 data "aws_iam_policy_document" "lambda_rw" {
@@ -84,7 +75,7 @@ data "aws_iam_policy_document" "lambda_rw" {
 
 resource "aws_iam_role_policy_attachment" "attach_rw" {
   policy_arn = aws_iam_policy.lamda_rw.arn
-  role       = aws_iam_role.lambda_function.name
+  role       = aws_iam_role.lambda_function_rw.name
 }
 
 
