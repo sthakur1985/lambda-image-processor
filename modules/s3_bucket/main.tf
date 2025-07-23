885255de2161061f71f9ee5d0b7e0a7d51cc01f7
sthakur1985 <104225376+sthakur1985@users.noreply.github.com>
2025-07-23 21:51:25 +0100

    Update main.tf

--- modules/s3_bucket/main.tf
+++ modules/s3_bucket/main.tf
@@ -1,1 +1,18 @@
 
+# modules/s3_bucket/main.tf
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_notification" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  lambda_function {
+    lambda_function_arn = var.lambda_arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_suffix       = ".jpg"
+  }
+
+  depends_on = [var.lambda_permission]
+}
+
