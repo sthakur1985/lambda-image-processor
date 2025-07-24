41d3544f2c5fc382a225ea27a76e91b20a92f3a7
sthakur1985 <104225376+sthakur1985@users.noreply.github.com>
2025-07-24 12:00:53 +0100

    Update variables.tf

--- modules/s3_bucket/variables.tf
+++ modules/s3_bucket/variables.tf
@@ -4,9 +4,15 @@ type = string
 }
 variable "lambda_arn" { 
 type = string 
+default = null
 }
 variable "lambda_permission" {
 type = any 
 description = "reference to lambda permission resource to check dependency"
+}
+
+variable "lambda_name" {
+type = string
+default = null
 }
 
