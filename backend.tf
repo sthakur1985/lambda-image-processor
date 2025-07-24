cf213d37b05b0675305506fab65982fffbdc41dd
sthakur1985 <104225376+sthakur1985@users.noreply.github.com>
2025-07-24 13:30:11 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "s3-exif-stripper/terraform.tfstate"
     region         = "eu-west-2"
     encrypt        = true
-    use_lockfile = true
+    use_lockfile = false
   }
 }
