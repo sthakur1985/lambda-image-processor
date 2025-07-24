02dee1b1703506e657f4ac77dd943670609cb14d
sthakur1985 <104225376+sthakur1985@users.noreply.github.com>
2025-07-24 15:58:53 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "bucket_a" {
   source         = "./modules/s3_bucket"
-  bucket_name    = "source-bucket-a-0725"
+  bucket_name    = var.bucket_name[0]
   lambda_arn     = module.lambda.lambda_arn
   lambda_permission = module.lambda.lambda_permission
   lambda_name = module.lambda.lambda_name
@@ -8,21 +8,21 @@ module "bucket_a" {
 
 module "bucket_b" {
   source      = "./modules/s3_bucket"
-  bucket_name = "destination-bucket-b-0725"
+  bucket_name = var.bucket_name[0]
   #lambda_arn  = "" # no lambda triggers
   lambda_permission = ""
 }
 
 module "lambda" {
   source            = "./modules/lambda_function"
-  function_name     = "exif-stripper"
+  function_name     = var.lambda_name
   role_arn          = module.iam.role_arn
   source_bucket_arn = module.bucket_a.bucket_arn
 }
 
 module "iam" {
   source        = "./modules/iam_roles"
-  name          = "exif-stripper-lambda-role-0725"
+  name          = var.lambda_role
   bucket_a_arn  = module.bucket_a.bucket_arn
   bucket_b_arn  = module.bucket_b.bucket_arn
 }
