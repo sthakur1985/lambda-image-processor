7168107633d21fc6b612edb1deb6f42633137ee8
sthakur1985 <104225376+sthakur1985@users.noreply.github.com>
2025-07-24 12:05:32 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "bucket_a" {
   source         = "./modules/s3_bucket"
   bucket_name    = "source-bucket-a-0725"
   lambda_arn     = module.lambda_function.lambda_arn
-  #lambda_permission = module.lambda_function.lambda_permission
+  lambda_permission = module.lambda_function.lambda_permission
   lambda_name = module.lambda_function.lambda_name
 }
 
