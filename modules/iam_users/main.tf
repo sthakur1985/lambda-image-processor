898069c1134a6c716ac5692583ef6091818b726c
sthakur1985 <104225376+sthakur1985@users.noreply.github.com>
2025-07-24 13:21:36 +0100

    Update main.tf

--- modules/iam_users/main.tf
+++ modules/iam_users/main.tf
@@ -1,3 +1,4 @@
+
 resource "aws_iam_user" "user_a" {
   name = "user-a"
 }
@@ -7,11 +8,39 @@ resource "aws_iam_user" "user_b" {
 }
 
 resource "aws_iam_user_policy" "user_a_policy" {
-  name = "user-a-bucket-policy"
+  name = "user-a-bucket-a-policy"
   user = aws_iam_user.user_a.name
 
   policy = jsonencode({
-   Verion = 
-  }
-  )
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:ListBucket"],
+        Resource = [
+          "arn:aws:s3:::${var.bucket_a_name}",
+          "arn:aws:s3:::${var.bucket_a_name}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy" "user_b_policy" {
+  name = "user-b-bucket-b-policy"
+  user = aws_iam_user.user_b.name
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = ["s3:GetObject", "s3:ListBucket"],
+        Resource = [
+          "arn:aws:s3:::${var.bucket_b_name}",
+          "arn:aws:s3:::${var.bucket_b_name}/*"
+        ]
+      }
+    ]
+  })
 }
