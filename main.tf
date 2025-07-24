492694b3d17ed3b18ff4f669e5687db33c06da53
sthakur1985 <104225376+sthakur1985@users.noreply.github.com>
2025-07-25 00:53:31 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,7 @@ module "lambda" {
   function_name     = var.lambda_name
   role_arn          = module.iam.role_arn
   source_bucket_arn = module.bucket_a.bucket_arn
+  source_bucket_name = module.bucket_a.bucket_name
 }
 
 module "iam" {
