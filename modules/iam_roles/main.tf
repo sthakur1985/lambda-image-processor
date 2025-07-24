3403126ebdf6dd28f0e9f9baa125dbb9e4a9037f
sthakur1985 <104225376+sthakur1985@users.noreply.github.com>
2025-07-25 00:29:37 +0100

    Update main.tf

--- modules/iam_roles/main.tf
+++ modules/iam_roles/main.tf
@@ -1,25 +1,29 @@
+# Assumed Role Policy for Lambda
 data "aws_iam_policy_document" "assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
-
     principals {
       type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
-
-    
     }
   }
 }
 
-resource "aws_iam_policy" "lambda_b_policy" {
-  name = "lambda-bucket-b-policy"
+# IAM Policy: Lambda can read/write to Bucket A
+resource "aws_iam_policy" "lambda_rw_bucket_a" {
+  name = "lambda-rw-bucket-a-policy"
 
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Effect   = "Allow",
-        Action   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:ListBucket"],
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ],
         Resource = [
           "arn:aws:s3:::${var.bucket_a_name}",
           "arn:aws:s3:::${var.bucket_a_name}/*"
@@ -29,17 +33,19 @@ resource "aws_iam_policy" "lambda_b_policy" {
   })
 }
 
- 
+# IAM Policy: Lambda can read from Bucket B
+resource "aws_iam_policy" "lambda_ro_bucket_b" {
+  name = "lambda-ro-bucket-b-policy"
 
-resource "aws_iam_policy" "lambda_a_policy" {
-  name = "lambda-bucket-a-policy"
-  
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Effect   = "Allow",
-        Action   = ["s3:GetObject", "s3:ListBucket"],
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ],
         Resource = [
           "arn:aws:s3:::${var.bucket_b_name}",
           "arn:aws:s3:::${var.bucket_b_name}/*"
@@ -49,21 +55,21 @@ resource "aws_iam_policy" "lambda_a_policy" {
   })
 }
 
+# IAM Role for Lambda function
 resource "aws_iam_role" "lambda_function" {
-  name = var.name
+  name               = "${var.name}-lambda-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-
-
-resource "aws_iam_role_policy_attachment" "attach_r" {
-  policy_arn = aws_iam_policy.lambda_a_policy.arn
+# Attach Read/Write policy to Lambda Role
+resource "aws_iam_role_policy_attachment" "attach_rw_bucket_a" {
   role       = aws_iam_role.lambda_function.name
+  policy_arn = aws_iam_policy.lambda_rw_bucket_a.arn
 }
 
-resource "aws_iam_role_policy_attachment" "attach_rw" {
-  policy_arn = aws_iam_policy.lambda_b_policy.arn
+# Attach Read-only policy to Lambda Role
+resource "aws_iam_role_policy_attachment" "attach_ro_bucket_b" {
   role       = aws_iam_role.lambda_function.name
+  policy_arn = aws_iam_policy.lambda_ro_bucket_b.arn
 }
-
 
