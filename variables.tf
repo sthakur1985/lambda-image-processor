ed6ff4e6ed804be0192cec2184bc236270207a44
sthakur1985 <104225376+sthakur1985@users.noreply.github.com>
2025-07-24 15:01:06 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+
+variable "bucket_name" { 
+  type = list(string) 
+  default = ["source-bucket-a-0725", destination-bucket-b-0725"]
+}
+
+variable "lambda_name" { 
+type = string 
+default = "exif-stripper"
+}
+
+variable "lambda_role" { 
+type = string 
+default = "exif-stripper-lambda-role-0725"
+}
