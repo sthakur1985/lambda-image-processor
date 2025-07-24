857f07443827dc897e2ed70f903893bc331d715d
sthakur1985 <104225376+sthakur1985@users.noreply.github.com>
2025-07-24 01:22:16 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "iam_user_a" {
-  value = module.iam_users.user_a_name
-}
+#output "iam_user_a" {
+#  value = module.iam_users.user_a_name
+#}
 
-output "iam_user_b" {
-  value = module.iam_users.user_b_name
-}
+#output "iam_user_b" {
+#  value = module.iam_users.user_b_name
+#}
